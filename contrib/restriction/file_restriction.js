d2666cc8f2b96df12816369372ad48bdb6ff534a
Miglius Alaburda <miglius@gmail.com>
2008-04-17 18:32:40 +0000

    Initial import of the fileframework module for D6

---
+++ contrib/restriction/file_restriction.js
@@ -0,0 +1,40 @@
+// $Id$
+
+/**
+ * Attach handlers to evaluate if the file extension is supported by the web server.
+ */
+
+Drupal.behaviors.file_extension = function(context) {
+  var translate = Drupal.settings.file_extension;
+  $("input[@type=file]", context).each(function() {
+
+    $(this).parent().after('<div class="file-extension error" style="display: block;">'+ translate.description +'<br />'+ translate.extension_allowed +'</div>');
+    $("div.file-extension").hide();
+
+    $(this).change(function() {
+
+      var file = $(this).val();
+      var ext = '';
+      if(file.length > 0) {
+        var dot = file.lastIndexOf(".");
+	if(dot != -1) {
+	  ext = file.substr(dot+1,file.length);
+	}
+      }
+      var extensions = translate.extension_allowed.split(" ");
+      var found = 0;
+      if(ext.length > 0) {
+        for(i=0;i<extensions.length;i++) {
+	  if(extensions[i] == ext.toLowerCase()) { found = 1; }
+        }
+      }
+      if(found == 1) {
+        $("div.file-extension").hide();
+      }
+      else {
+        $("div.file-extension").show();
+      }
+    }); 
+  });
+};
+
---
+++ file.js
@@ -0,0 +1,10 @@
+// $Id$
+
+Drupal.behaviors.fileUpload = function() {
+  $('input[@type=file]').change(function() {
+    if (this.value != '' && $('#edit-title').val() == '') {
+      var title = this.value.match(/([^\/\\]+)$/)[1];
+      $('#edit-title').val(title);
+    }
+  });
+};
